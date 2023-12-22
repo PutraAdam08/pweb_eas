e4e02bf682ef01518b21c2e6a435dcdd5375ba30
MoChikayo <127732169+MoChikayo@users.noreply.github.com>
2023-12-22 20:35:14 +0700

    Commnit

---
+++ pweb_eas/pweb_eas/Pages/Recipe.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace pweb_eas.Pages
+{
+    public class RecipeModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
